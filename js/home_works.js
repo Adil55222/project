d8e4e843fbfac08ab7a5d0da9b285c02a33c33b0
Adil55222 <eldiarovadilbek7@gmail.com>
2025-01-15 17:42:30 +0600

    DZ2

--- js/home_works.js
+++ js/home_works.js
@@ -21,18 +21,66 @@ button.onclick=function (){
 
 const child_block =document.querySelector(".child_block")
 const parent_block = document.querySelector(".parent_block")
-const parent = parent_block.clientWidth - child_block.clientWidth
+const width  = parent_block.clientWidth - child_block.clientWidth
+const height = parent_block.clientHeight - child_block.clientHeight
 
 let x = 0
+let y = 0
 
 function move() {
-    x+=1
-    child_block.style.left = x+"px"
-    if (x<parent) {
+
+    if (x < width && y === 0) {
+        x+=1
+        child_block.style.left = x+"px"
         requestAnimationFrame(move)
 
+    }else if ( x === width && y < height ) {
+        y+=1
+        child_block.style.top = y+"px"
+        requestAnimationFrame(move)
+    }else if ( x > 0 && y === height) {
+        x-=1
+        child_block.style.left = x+"px"
+        requestAnimationFrame(move)
+    }else if ( x===0 && y >0 ) {
+        y -=1
+        child_block.style.top = y+"px"
+        requestAnimationFrame(move)
     }
 }
 move()
 
+// todo StopWatch
 
+const seconds = document.querySelector('#seconds')
+const start = document.querySelector('#start')
+const stop = document.querySelector('#stop')
+const reset = document.querySelector('#reset')
+
+let count = 0
+let interval
+
+// start.addEventListener( 'click', ()=>{
+//     console.log(55)
+//
+// })
+
+start.onclick = function(){
+    if(!interval){
+        interval = setInterval(()=>{
+            count++
+            seconds.innerHTML = count
+        }, 1000)
+    }
+
+}
+
+stop.onclick = function(){
+    clearInterval(interval)
+}
+
+reset.onclick = function(){
+    clearInterval(interval)
+    count = 0
+    seconds.innerHTML = count
+}
